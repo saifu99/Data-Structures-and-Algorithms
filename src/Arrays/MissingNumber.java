50f3fef175b26c891a35460c435617744680c4ee
saifu99 <trksaifuyt@gmail.com>
2025-11-02 21:45:08 +0530

    modified

--- src/Arrays/MissingNumber.java
+++ src/Arrays/MissingNumber.java
@@ -1,4 +1,19 @@
-public class MissingNumber {
+public class Arrays_MissingNumInArray {
+    public static int missingNumber(int[] nums, int n){   //brute force
+        for (int i=1; i<=n; i++){
+            int flag=0;
+
+            for (int j=0; j<n-1; j++){
+                if (nums[j] == i){
+                    flag=1;
+                    break;
+                }
+            }
+            if (flag == 0) return i;
+        }
+        return -1;
+    }
+
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int expectedSum = (n * (n + 1)) / 2; //sum of all numbers including missing number
@@ -8,5 +23,14 @@ public class MissingNumber {
             actualSum += nums[i];
         }
         return expectedSum - actualSum;
+    }
+}
+
+    public static void main(String[] args){
+        int n=5;
+        int[] nums = {1,2,4,5};
+
+        int ans = (nums,n);
+        System.out.println("The missing number is: "+ans);
     }
 }
