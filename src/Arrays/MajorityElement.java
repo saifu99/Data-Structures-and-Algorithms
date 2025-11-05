7c5868251ef01a831b5474028c3aef3954fc4cb7
saifu99 <trksaifuyt@gmail.com>
2025-11-05 23:58:46 +0530

    Leetcode 169

---
+++ src/Arrays/MajorityElement.java
@@ -0,0 +1,18 @@
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count=0, candidate=0;
+
+        for (int i=0; i<nums.length; i++) {
+            int number = nums[i];
+            if (count == 0) {
+                candidate = number;
+            }
+            if (number == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
