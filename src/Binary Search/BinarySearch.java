06ac7dda1993ca05a13d8dd9228f4ada34bafe1a
saifu99 <trksaifuyt@gmail.com>
2025-12-01 21:52:22 +0530

    Leetcode 704 added

---
+++ src/Binary Search/BinarySearch.java
@@ -0,0 +1,23 @@
+//Leetcode 704
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(search(nums,9));
+    }
+}
