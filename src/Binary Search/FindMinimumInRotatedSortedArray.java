46a319d0184c037f4553640a3c3da08f25ca520c
saifu99 <trksaifuyt@gmail.com>
2025-12-04 22:06:26 +0530

    Leetcode 153

--- src/Binary Search/FindMinimumInRotatedSortedArray.java
+++ src/Binary Search/FindMinimumInRotatedSortedArray.java
@@ -1,19 +1,16 @@
 //Leetcode 153
 public class FindMinimumInRotatedSortedArray {
-    static int finMinBruteForce(int[] arr) {   //Brute force
-        int n = arr.length;
-        int mini = Integer.MAX_VALUE;
+    static int finMinBruteForce(int[] nums) {
+        int n = nums.length, mini = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            mini = Math.min(mini, arr[i]);
+            mini = Math.min(mini, nums[i]);
         }
         return mini;
     }
 
-
-
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int[] arr = {4,5,6,7,0,1,2};
         System.out.println("The minimum element is: "+finMinBruteForce(arr));
     }
 }
