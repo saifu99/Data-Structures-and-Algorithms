43a9aeb1c13d26d18c6377f51ef886b53f2ee798
saifu99 <trksaifuyt@gmail.com>
2025-12-04 22:17:37 +0530

    Leetcode 153 optimal solution

--- src/Binary Search/FindMinimumInRotatedSortedArray.java
+++ src/Binary Search/FindMinimumInRotatedSortedArray.java
@@ -8,9 +8,23 @@ public class FindMinimumInRotatedSortedArray {
         }
         return mini;
     }
-    
+
+    public static int finMinOptimal(int[] nums){
+        int left=0, right=nums.length-1;
+
+        while (left<right){ 
+            int mid=left+(right-left)/2;
+            if(nums[mid] > nums[right]){
+                left=mid+1;
+            }else {
+                right=mid;
+            }
+        }
+        return nums[left];
+    }
+
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        System.out.println("The minimum element is: "+findMinBruteForce(arr));
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println("The minimum element is: "+finMinOptimal(nums));
     }
 }
