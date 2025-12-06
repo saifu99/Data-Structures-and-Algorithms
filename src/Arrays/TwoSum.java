21b27d6c3ec18d72ae6a956449b16c4db0633f56
saifu99 <trksaifuyt@gmail.com>
2025-12-06 20:42:19 +0530

    upgrade changes in two sum problem leetcode

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -5,8 +5,7 @@ public class TwoSum {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 if(nums[i]+nums[j]==target){
-                    int arr[]={i,j};
-                    return arr;
+                    return new int[]{i,j};
                 }
             }
         }
@@ -14,7 +13,6 @@ public class TwoSum {
     }
 
     static void main(String[] args) {
-//        TwoSum solution = new TwoSum();
         int[] nums = {2,7,11,15};
         System.out.println(Arrays.toString(twoSum(nums,9)));
     }
