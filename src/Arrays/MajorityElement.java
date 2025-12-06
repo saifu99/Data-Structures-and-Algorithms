e4494550152a969871db77609790ee14e73d50f3
saifu99 <trksaifuyt@gmail.com>
2025-12-06 21:40:05 +0530

    Leetcode 169 change non-static method to static

--- src/Arrays/MajorityElement.java
+++ src/Arrays/MajorityElement.java
@@ -1,5 +1,5 @@
 public class MajorityElement {
-    public int majorityElement(int[] nums) {
+    public static int majorityElement(int[] nums) {
         int count=0, candidate=0;
 
         for (int i=0; i<nums.length; i++) {
@@ -17,7 +17,7 @@ public class MajorityElement {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println("The majority element is: " + new MajorityElement().majorityElement(arr));
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums));
     }
 }
